60aedd4fe4b426ee132b4a6d5b14b370db6013bf
Peter Sam <sam.krealogi@gmail.com>
2022-04-18 11:39:09 +0700

    other page

---
+++ lib/ui/pages/transaction_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class TransactionPage extends StatelessWidget {
+  const TransactionPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Transaction Page'),
+    );
+  }
+}
